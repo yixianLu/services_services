6dab266fb1e6e58797bd91edd3784579f1496f68
KevinXu17 <xuzhaozhishu@gmail.com>
2023-03-14 20:14:00 -0700

    Change the pi1 and pi2 host in hello service so it can run in local docker

--- microservices/services/hello/hello.py
+++ microservices/services/hello/hello.py
@@ -4,18 +4,20 @@ import json
 import os
 
 app = Flask(__name__)
-
-localhost = 'localhost'
+# TODO local host need to change to microservice request
+pi1_network = 'pi1_network'
+pi2_network = 'pi2_network'
 
 @app.route('/')
 def get_data():
-    pi1_get = requests.get('http://localhost:8000/pi1')
+    pi1_get = requests.get('http://pi1_network:8000/pi1')
     pi1_text = json.loads(pi1_get.text)
-    pi2_get = requests.get('http://localhost:8081/pi2')
+    pi2_get = requests.get('http://pi2_network:8081/pi2')
     pi2_text = json.loads(pi2_get.text)
     pi_List = pi1_text+pi2_text
     return jsonify(pi_List),200
 
 
 if __name__=='__main__':
-    app.run(host=localhost, port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=5000, debug=True)
