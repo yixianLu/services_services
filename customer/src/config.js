ced4c9012d5185dcd8736e0fec05657e5fcea8ae
KevinXu17 <xuzhaozhishu@gmail.com>
2023-03-28 10:28:32 -0700

    .

--- customer/src/config.js
+++ customer/src/config.js
@@ -5,8 +5,8 @@ module.exports = {
         endpoint: 'http://localhost:3333'
     },
     aws_remote_config: {
-        accessKeyId: 'AKIAV6KEXDHVZ7E32MUU',
-        secretAccessKey: 'ev7vyIJ9OCl2Q4knHBTsGOl09o+hy+lWUPEWfTlm',
+        accessKeyId: 'YOUR',
+        secretAccessKey: 'YOUR',
         region: 'us-east-1'
     }
 };
